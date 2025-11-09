4159003392ba2086ca4ad5cd53c59bd8e3b25a26
LAPTOP-9Q8IDV0S\Marek <orihel8@gmail.com>
2025-11-09 01:05:15 +0100

    fixed API_BASE_URL

--- frontend/services/apiConfig.ts
+++ frontend/services/apiConfig.ts
@@ -1,2 +1,4 @@
-export const API_BASE_URL = "https://localhost:5058/api"; // test
-//export const API_BASE_URL = "https://localhost:5058/api"; // prod
+// export const API_BASE_URL = "https://localhost:5058/api"; // test
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "https://marekorihel.com/api";
+ // prod
